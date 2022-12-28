604b63773d4ccedf5bbc937347077588250de365
jwhong <jwhong98@gmail.com>
2022-12-27 16:17:30 -0800

    styled searchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,24 @@
 import React from "react";
+import classes from "./SearchBar.module.css";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div className={classes.searchContainer}>
+      <form className={classes.searchForm} action="/">
+        <input
+          type="text"
+          name="searchItem"
+          id="searchItem"
+          className={classes.input}
+          placeholder="Search..."
+        />
+        <button type="submit" id={classes.submitBtn}>
+          <ion-icon name="search"></ion-icon>
+        </button>
+      </form>
+      <button>shopIcon</button>
+    </div>
+  );
 };
 
 export default SearchBar;
