09bea1586a73290a96daea71b339ea4f9bb30c7f
jwhong <jwhong98@gmail.com>
2023-01-09 14:20:41 -0800

    InfoNav tabs now have active state

--- src/components/InfoNav/InfoNav.js
+++ src/components/InfoNav/InfoNav.js
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./InfoNav.module.css";
-import Tab from "./Tab";
 
 const InfoNav = () => {
+  const [isActive, setIsActive] = useState("na");
+  const onClickHandler = (e) => {
+    setIsActive(e.target.dataset.value);
+  };
   return (
     <div className={classes.infoNavContainer}>
       <nav className={classes.infoNav}>
-        <Tab label="new arrivals" active={true} />
-        <Tab label="restocks" />
-        <Tab label="future" />
-        <Tab label="featured" />
-        <Tab label="sale" />
+        <div
+          className={`${classes.tab} ${
+            isActive === "na" ? classes.active : ""
+          }`}
+          onClick={onClickHandler}
+          data-value="na"
+        >
+          new arrivals
+        </div>
+        <div
+          className={`${classes.tab} ${
+            isActive === "re" ? classes.active : ""
+          }`}
+          onClick={onClickHandler}
+          data-value="re"
+        >
+          restocks
+        </div>
+        <div
+          className={`${classes.tab} ${
+            isActive === "fu" ? classes.active : ""
+          }`}
+          onClick={onClickHandler}
+          data-value="fu"
+        >
+          future
+        </div>
+        <div
+          className={`${classes.tab} ${
+            isActive === "fe" ? classes.active : ""
+          }`}
+          onClick={onClickHandler}
+          data-value="fe"
+        >
+          featured
+        </div>
+        <div
+          className={`${classes.tab} ${
+            isActive === "sa" ? classes.active : ""
+          }`}
+          onClick={onClickHandler}
+          data-value="sa"
+        >
+          sale
+        </div>
       </nav>
     </div>
   );
--- src/components/InfoNav/Tab.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import classes from "./Tab.module.css";
-
-const Tab = (props) => {
-  const active = props.active ? classes.active : "";
-  return <div className={`${classes.tab} ${active}`}>{props.label}</div>;
-};
-
-export default Tab;
