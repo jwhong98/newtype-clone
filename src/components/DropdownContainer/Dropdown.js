f5711a7dcaeb3d6a8b57884cb85ea389e2e4ec85
jwhong <jwhong98@gmail.com>
2023-01-12 14:34:26 -0800

    init dropdown implementation

--- src/components/DropdownContainer/Dropdown.js
+++ src/components/DropdownContainer/Dropdown.js
@@ -2,7 +2,14 @@ import React from "react";
 import classes from "./Dropdown.module.css";
 
 const Dropdown = (props) => {
-  return <div className={classes.dropdown}>{props.label}</div>;
+  return (
+    <>
+      <div className={classes.dropdownContainer}>
+        <div className={classes.dropdown}>{props.label}</div>
+        <div className={classes.dropdownMenu}>DROPDOWN</div>
+      </div>
+    </>
+  );
 };
 
 export default Dropdown;
